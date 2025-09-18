6e47e6e25e6a60d6121f1460099a26979a5f8ce0
Daniel Tyroller <72881833+danieltyroller@users.noreply.github.com>
2025-09-18 11:13:58 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+<script>
+  window.SUPABASE_CONFIG = {
+    url: 'https://ccdmkvidlsaqidskdgcd.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjZG1rdmlkbHNhcWlkc2tkZ2NkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MDEyOTMsImV4cCI6MjA2NTk3NzI5M30.P5KowUxOMjsqY0SGb04TwGiwhm-H-uaY3NIp67HKbR8'   // <-- dein kompletter anon key
+  };
+</script>
